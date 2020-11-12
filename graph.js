1007ccdbc2f32283d1462c8ed03402fa93c7c8a5
nickthorpe71 <nickthorpe71@gmail.com>
2020-11-12 14:18:58 -0600

    adds initial graph setup

--- graph.js
+++ graph.js
@@ -1,1 +1,13 @@
-console.log("test");
+const airports = 'PHX BKK OKC JFK LAX MEX EZE HEL LOS LAP LIM'.split(' ');
+
+// the graph
+const adjacencyList = new Map();
+
+function addNoce(airport){
+	adjacencyList.set(airport, []);
+}
+
+function addEdge(origin, destination){
+	adjacencyList.get(origin).push(destination);
+	adjacencyList.get(destination).push(origin);
+}
